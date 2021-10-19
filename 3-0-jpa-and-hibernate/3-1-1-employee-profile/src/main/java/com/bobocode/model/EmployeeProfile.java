066e02fbd0d5f0b6307e6246a127094a9d6dd906
skolach <ssskolach@gmail.com>
2021-10-19 11:51:34 +0300

    111

--- 3-0-jpa-and-hibernate/3-1-1-employee-profile/src/main/java/com/bobocode/model/EmployeeProfile.java
+++ 3-0-jpa-and-hibernate/3-1-1-employee-profile/src/main/java/com/bobocode/model/EmployeeProfile.java
@@ -1,5 +1,11 @@
 package com.bobocode.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,9 +22,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "employee")
 public class EmployeeProfile {
+    @Id
+    @GeneratedValue
     private Long id;
+    @Column(nullable = false)
     private Employee employee;
+    @Column(nullable = false)
     private String position;
+    @Column(nullable = false)
     private String department;
 }
